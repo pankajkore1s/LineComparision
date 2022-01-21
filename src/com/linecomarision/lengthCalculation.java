0cf74d7bd586aa29dec1aee8b44dbe0a248fa688
pankaj kore <korep95@gmail.com>
2022-01-21 10:16:13 +0530

    [pankaj]added the calculation of length

---
+++ src/com/linecomarision/lengthCalculation.java
@@ -0,0 +1,5 @@
+package com.linecomarision;
+
+public class lengthCalculation {
+
+}
--- src/com/linecomarision/lineComparision.java
+++ src/com/linecomarision/lineComparision.java
@@ -1,7 +1,25 @@
 package com.linecomarision;
 
+import java.util.Scanner;
+
 public class lineComparision {
-		public static void main (String []args) {
-			System.out.println("welcome to the line comparision computation");
-		}
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation !!");
+
+		Scanner sc=new Scanner(System.in);
+		System.out.print("plese Enter line co-ordinates x1=");
+		int x1=sc.nextInt();
+		System.out.print("plese Enter line co-ordinates x2=");
+		int x2=sc.nextInt();
+		System.out.print("plese Enter line co-ordinates y1=");
+		int y1=sc.nextInt();
+		System.out.print("plese Enter line co-ordinates x2=");
+		int y2=sc.nextInt();
+		double length_of_line=Math.sqrt((x2-x1)^2+(y2-y1)^2);
+		sc.close();
+		System.out.println("Length of The line is="+length_of_line);
+
+	}
 }
+
+
