2fe6693514b9a0d42ba19d4de27c699fec4c762b
pankaj kore <korep95@gmail.com>
2022-01-21 08:21:20 +0530

    [pankaj]added welcome message

---
+++ src/com/linecomarision/lineComparision.java
@@ -0,0 +1,7 @@
+package com.linecomarision;
+
+public class lineComparision {
+		public static void main (String []args) {
+			System.out.println("welcome to the line comparision computation");
+		}
+}
